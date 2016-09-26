c3bde29c1b683a85c8c64f8d93ee9367e1ec7c9b
Rushabh Mehta <rmehta@gmail.com>
2016-09-26 17:44:33 +0530

    fix date format for validation

--- shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.js
+++ shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.js
@@ -2,9 +2,10 @@ frappe.ready(function() {
 	// bind events here
 	$('input[name="date_of_birth"][data-doctype="Student Applicant"]').on('change', function() {
 		var date = $(this).val();
-		if (moment(date) < moment('2013-09-01') || moment(date) > moment('2014-08-30')) {
+		if (moment(date, moment.defaultFormat)
+			< moment('2013-09-01') || moment(date, moment.defaultFormat) > moment('2014-08-30')) {
 			frappe.msgprint("Child does not meet age criteria for this year's admission")
 			$(this).val('');
 		}
 	});
-})
+});
