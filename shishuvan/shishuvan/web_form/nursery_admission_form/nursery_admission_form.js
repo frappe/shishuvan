eb157015593e2bcf8bd216c36a2d7c80228c13bd
Rushabh Mehta <rmehta@gmail.com>
2016-09-26 17:05:06 +0530

    [fix] fixes to nursery admissions

--- shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.js
+++ shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.js
@@ -1,10 +1,10 @@
 frappe.ready(function() {
 	// bind events here
-	$('input[name="date_of_birth"]').on('change', function() {
+	$('input[name="date_of_birth"][data-doctype="Student Applicant"]').on('change', function() {
 		var date = $(this).val();
 		if (moment(date) < moment('2013-09-01') || moment(date) > moment('2014-08-30')) {
 			frappe.msgprint("Child does not meet age criteria for this year's admission")
 			$(this).val('');
 		}
-	})
+	});
 })
