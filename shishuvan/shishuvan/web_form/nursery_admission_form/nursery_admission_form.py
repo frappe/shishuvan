a1906b95a63189a0e54653ead060c3768571edd4
Rushabh Mehta <rmehta@gmail.com>
2016-09-28 15:08:49 +0530

    [fix] list view for form

--- shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.py
+++ shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.py
@@ -3,5 +3,11 @@ from __future__ import unicode_literals
 import frappe
 
 def get_context(context):
-	# do your magic here
-	pass
+	'''Update context'''
+
+	# make form read-only if paid
+	if context.doc and context.doc.paid:
+		context.read_only = 1
+
+def get_list_context(context):
+	context.row_template = 'shishuvan/templates/includes/applicant_row.html'
