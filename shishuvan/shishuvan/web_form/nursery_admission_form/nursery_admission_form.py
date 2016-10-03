7defc8854b12a28c776ad25bdc6ff9441dbcb67b
Rushabh Mehta <rmehta@gmail.com>
2016-10-03 15:52:09 +0530

    [fix] add filters to applications

--- shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.py
+++ shishuvan/shishuvan/web_form/nursery_admission_form/nursery_admission_form.py
@@ -11,3 +11,4 @@ def get_context(context):
 
 def get_list_context(context):
 	context.row_template = 'shishuvan/templates/includes/applicant_row.html'
+	context.filters = {'program': 'Nursery'}
--- shishuvan/shishuvan/web_form/preliminary_application_form/preliminary_application_form.py
+++ shishuvan/shishuvan/web_form/preliminary_application_form/preliminary_application_form.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 
-import frappe
+import frappe, json
 
 def get_context(context):
 	# do your magic here
 	pass
+
+def get_list_context(context):
+	context.row_template = 'shishuvan/templates/includes/applicant_row.html'
+	context.filters = {'program': ('!=', 'Nursery')}
