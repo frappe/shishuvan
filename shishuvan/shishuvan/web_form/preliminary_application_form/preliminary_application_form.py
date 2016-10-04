ea945b43f052ebb1ba2dd9a61016d368a2a31ba3
Rushabh Mehta <rmehta@gmail.com>
2016-10-04 16:25:26 +0530

    activate payment for forms

--- shishuvan/shishuvan/web_form/preliminary_application_form/preliminary_application_form.py
+++ shishuvan/shishuvan/web_form/preliminary_application_form/preliminary_application_form.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 import frappe, json
 
 def get_context(context):
-	# do your magic here
-	pass
+	# make form read-only if paid
+	if context.doc and context.doc.paid:
+		context.read_only = 1
 
 def get_list_context(context):
 	context.row_template = 'shishuvan/templates/includes/applicant_row.html'
